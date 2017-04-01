58444b3f4bf0956e5c9b568aec621fa3a4dbe7aa
yoadad <yoadad@gmail.com>
2017-04-01 14:14:55 -0500

    Adding XF.alert and XF.confirm functions
    Adding missing images

--- XF/Scripts/main.js
+++ XF/Scripts/main.js
@@ -1,6 +1,8 @@
 ﻿var XF = XF || {};
 
 (function ($, XF) {
+    XF.confirmResult = false; 
+    XF.confirmOnClose = function(){};
     XF.logData = {};
     XF.addInfoMessage = function (message, type) {
         var type = type || 'info';
@@ -36,5 +38,72 @@
         return html;
     };
 
+    XF.alert = function (text,type,icon) {
+        var html = XF.getHtmlFromTemplate('#alertTemplate',
+        {
+            text: text,
+            type: type || 'info',
+            icon: icon || 'glyphicon-info-sign'
+        });
+        $('#alert .content').html(html);
+        XF.alertWindow.center().open();
+    };
+
+    XF.confirm = function (text,fn) {
+        XF.confirmOnClose = fn;
+        var html = XF.getHtmlFromTemplate('#confirmTemplate',{text: text});
+        $('#confirm .content').html(html);
+        XF.confirmWindow.center().open();
+
+    };
+
+    //Common controlls
+
+    //Alert window
+    XF.alertWindow = $('#alert').kendoWindow({
+        width: "600px",
+        title: "Application Message",
+        visible: false,
+        modal:true,
+        actions: [
+            "Close"
+        ],
+    }).data("kendoWindow");
+
+    $('#btnAlertOK').on('click', function () {
+        XF.alertWindow.close();
+    });
+
+    //Confirm window
+    XF.confirmWindow = $('#confirm').kendoWindow({
+        width: "500",
+        title: "Application Message",
+        visible: false,
+        modal: true,
+        actions: [
+            "Close"
+        ],
+    }).data("kendoWindow");
+
+    $('#btnConfirmCancel').on('click', function () {
+        XF.confirmWindow.close();
+    });
+    $('#btnConfirmOK').on('click', function () {
+        XF.confirmWindow.close();
+        XF.confirmOnClose();
+    });
+
+    //kendoWindow({
+    //    width: "600px",
+    //    title: "About Alvar Aalto",
+    //    visible: false,
+    //    actions: [
+    //        "Pin",
+    //        "Minimize",
+    //        "Maximize",
+    //        "Close"
+    //    ],
+    //    close: onClose
+    //}).data("kendoWindow").center().open();
 
 }(jQuery,XF));
