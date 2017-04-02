fd29bce215fb50f487f2d9b4930d790cf2fb7a6c
yoadad <yoadad@gmail.com>
2017-04-02 12:22:13 -0500

    Fixing products list

--- XF/Scripts/Products/ProductsList.js
+++ XF/Scripts/Products/ProductsList.js
@@ -4,31 +4,6 @@
 
     var baseUrl = document.URL;
 
-    XF.SetStock = function (productId,stock) {
-        var url = '/Products/SetStock';
-        var data = { productId: productId, stock: stock };
-        $.post(url,data, XF.SetStockResponse,'json');
-    };
-
-    XF.SetStockResponse = function (data) {
-        if (data.Result) {
-            XF.addInfoMessage('Stock updated');
-        }
-        else {
-            XF.addInfoMessage(data.Message,'danger');
-        }
-    };
-
-    $('#btnUpdate').on('click', function () {
-        if (confirm('This action is going to update the current product stock, are you sure that you want to continue?')) {
-            var productId = $('#hfProductId').val();
-            var stock = $('#txtStock').val();
-            console.log(productId + ','+ stock);
-            
-            XF.SetStock(productId, stock);
-        }
-    });
-
     var _dataSource = new kendo.data.DataSource({
         transport: {
             read:
@@ -43,12 +18,12 @@
     });
 
     console.log(_dataSource);
-   
+
     $("#grid").kendoGrid({
         dataSource: _dataSource,
         sortable: true,
         pageable: true,
-        filterable:{
+        filterable: {
             extra: false,
             operators:
                 {
@@ -62,7 +37,7 @@
         //selectable: "multiple cell",
         columns:
             [
-                { field: "Id"},
+                { field: "Id" },
                 {
                     field: "Code"
                 },
