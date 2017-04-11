870b807df365b5a06b153a26e75c6915a99b2906
BDT\BMXEXT011096 <vargas.jesus1984@gmail.com>
2017-04-11 12:46:27 -0500

    Feature: currently this add the product entered into the displayed grid.

--- XF/Scripts/Invoices/SalesPoint.js
+++ XF/Scripts/Invoices/SalesPoint.js
@@ -1,18 +1,42 @@
 ﻿var XF = XF || {};
 
 (function ($, XF) {
+    var baseUrl = document.URL;
     XF.CurrentProduct = {};
     XF.addConcept = function (product) {
-        var newConcept = XF.getHtmlFromTemplate('#invoiceConcept', product);
-        $('#invoiceTable').append(newConcept);
+        var gridDataSource = $('#grid').data('kendoGrid').dataSource;
+        gridDataSource.add(product);
+        gridDataSource.sync();
+        console.log(gridDataSource);
     };
 
     $('#btnAdd').on('click', function () {
         var product = $.extend({
-            Units: $('#txtUnits').val()
+            Units: $('#txtUnits').val(),
+            Concept: $('#txtProduct').val(),
+            Price: 0.00,
+            Total: 0.00
         }, XF.CurrentProduct);
-        XF.addConcept(product);
+
+        
+        $.ajax({
+            url: baseUrl + '/GetProduct',
+            method: 'GET',
+            dataType: 'json',
+            data: { concept: JSON.stringify(product.Concept) }
+        })
+        .done(function (data) {
+            //console.log(data.products);
+            product.Price = data.products.PurchasePrice;
+            product.Total = product.Units * product.Price;
+            XF.addConcept(product);
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log('Error status: ' + textStatus + ' ' + jqXHR.statusText())
+        });
     });
+
+
 
     var productAutocomplete = $('#txtProduct').kendoAutoComplete({
         dataTextField: "Name",
@@ -30,6 +54,28 @@
             console.log(e);
         }
     });
+    
+    var products = new kendo.data.DataSource({
+     
+    });
+    function onDataBonDataBindingound(e)
+    {
+        alert(e.model.Concept);
+    }
+    //declaring the grid structure
+    $('#grid').kendoGrid({
+        dataSource: products,
+        //dataBinding: onDataBinding,
+        columns:
+            [
+                { field: "Id", hidden: true },
+                { field: "Concept" },
+                { field: "Units" },
+                { field: "Price" },
+                { field: "Total" }
 
+                //{field:"footer", width:100, footerTemplate:'<p>MyFooter</p>'}
+            ]
+    });
 
 })(jQuery, XF);
